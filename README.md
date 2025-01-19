12cdfce35915aeff68189a58d96f0551cef3f684
pratikgit281998 <pratik.nandapurkar17@gmail.com>
2025-01-20 02:19:12 +0530

    added req

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Hate_Speech_Detection
-End to end hate speech classification
+<!-- End to end hate speech classification -->
+
+<!-- need to create env -->
+# How to run
+
+'''bash
+conda create -n Hate_Speech_Detection python = 3.12.6 -y
+'''
+
+
+'''bash
+conda activate HATE_SPEECH_DETECTION
+'''
+
+'''bash
+pip install -r requirements.txt
+'''
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "HATE_SPEECH_DETECTION",
+    version = "0.0.1",
+    author = "pratik",
+    author_email = "pratik.nandapurkar17@gmail.com",
+    packages = find_packages(),
+    install_requires=[]
+)
+
+#this setup file will figure out constructor in every folder and it will install that folder as local package
